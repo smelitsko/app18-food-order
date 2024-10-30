a712ab6467a3f606b9186711c003faac3deaf5c4
Silvana Melitsko <melitsko067@est.derecho.uba.ar>
2024-10-30 18:05:30 -0300

    Day 57-Frontend y bootstrap

--- menu_restaurant/admin.py
+++ menu_restaurant/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Item
+
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status")
+    search_fields = ("meal", "description")
+    list_filter = ("status",)
+
+
+admin.site.register(Item, MenuItemAdmin)
 
 # Register your models here.
--- menu_restaurant/views.py
+++ menu_restaurant/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Item, MEAL_TYPE
 
-from django.shortcuts import render
+
+class MenuList(generic.ListView):
+    queryset = Item.objects.order_by("date_created")
+    template_name = "index.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
+        return context
 
 
-def index(request):
-    return render("index.html")
+class MenuItemDetail(generic.DetailView):
+    model = Item
+    template_name = "menu_item_detail.html"
